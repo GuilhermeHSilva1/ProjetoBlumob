68d6fd3c3821003602049aa2f08c02a38ea1ba55
DESKTOP-6JOQQDV\Guilh <GuilhermeHillesheim387@gmail.com>
2023-03-08 02:46:47 -0300

    Atualizar saldo da tela da Catraca

--- Projeto/frontend/Views/UFraCatraca.pas
+++ Projeto/frontend/Views/UFraCatraca.pas
@@ -50,8 +50,8 @@ end;
 
 procedure TfraCatraca.UsarCatraca;
 begin
-    FServico.UsarCatraca;
-    lblSaldo.Text := 'Saldo Atual: R$:' + FormatFloat('0.00', FServico.Cartao.Saldo);
+  FServico.UsarCatraca;
+  lblSaldo.Text := 'Saldo Atual: R$:' + FormatFloat('0.00', FServico.Cartao.Saldo);
 end;
 
 end.
